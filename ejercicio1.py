407a039b961c64d25ef9824f57f8d78a8a60a659
loxi25 <oscarsanchez1@colegioguanenta.edu.co>
2025-03-12 07:08:22 -0500

    se crearon nuevos archivos

---
+++ ejercicio1.py
@@ -0,0 +1,12 @@
+def mostrarcadena(cadena, n):
+    for i in range(1, n + 1):
+        print(f"{i} . {cadena}")
+
+
+cadena=input("dijite la cadena a mostrar")
+n = input("dijite el numero: ")
+
+mostrarcadena(cadena, n)
+
+print("\nEso era...")
+
---
+++ ejericicio.py
@@ -0,0 +1,14 @@
+print("==========================")
+print("mostrar cadena n veces en ")
+print("==========pantalla========")
+
+def mostrarnombre(nombre):
+    for i in range(1, 6):
+        print(f"{i} . {nombre}")
+
+nombre = input("dijite su nombre: ")
+
+mostrarnombre(nombre)
+
+print("\nEso era...")
+
